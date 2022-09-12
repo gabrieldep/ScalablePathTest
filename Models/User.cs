7e6dc1d80bd0443c38a5a1cc1cdfa5885e871ecf
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-09-12 15:24:34 -0300

    Dateof birth nullable

--- Models/User.cs
+++ Models/User.cs
@@ -3,7 +3,7 @@
     public class User
     {
         public int Id { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
         public string? LeadSource { get; set; }
         public string? Name { get; set; }
     }
