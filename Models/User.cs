6bc1f0fab30def6df1eb6e6cf52076f4904d69cb
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-09-12 14:57:04 -0300

    Add: user model

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ScalablePathTest.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? LeadSource { get; set; }
+        public string? Name { get; set; }
+    }
+}
